f1266ca10da4669dd0c5c1e20892104f179f26ec
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-10-23 16:40:24 +0200

    feat: add pagination comments

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,7 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="container mt-5">
+    Lista
+</div>
     <div class="container mt-5">
         @dump($projects)
+
+        {{-- {{ $posts->links('pagination::bootstrap-5')}} --}}
     </div>
 @endsection
