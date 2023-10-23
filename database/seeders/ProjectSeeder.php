657d617e633df4bf5be310674c553231d6e9f74e
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-10-23 15:26:16 +0200

    feat: add seeder contets

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,8 +2,14 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
+
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+
+use Illuminate\Support\Str;
+
+use Faker\Generator as Faker;
 
 class ProjectSeeder extends Seeder
 {
@@ -12,8 +18,16 @@ class ProjectSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 10; $i++) {
+            $project = new Project();
+
+            $project->author = $faker->firstNameMale();
+            $project->title = $faker->catchPhrase();
+            $project->date = $faker->dateTime();
+            $project->description = $faker->paragraphs(2, true);
+            $project->slug = Str::slug($project->title);
+        }
     }
 }
